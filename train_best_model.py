978cf265c2ec944d04ddac6d637c57533e171df2
Esteban Juarez <58830440+ElStevie@users.noreply.github.com>
2022-10-07 01:27:05 -0500

    Update training for best model

--- train_best_model.py
+++ train_best_model.py
@@ -1,7 +1,23 @@
+import pandas as pd
 from keras.callbacks import ModelCheckpoint
+from matplotlib import pyplot as plt
 
 from mobilenetv2tuner import MobileNetV2Tuner
 from utils import get_train_generator, get_validation_generator
+
+
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    plt.figure()
+    plt.title('Loss vs Iterations')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.plot(hist['epoch'], hist['loss'], label='Train Error')
+    plt.plot(hist['epoch'], hist['val_loss'], label='Val Error')
+    plt.legend()
+    plt.savefig('Results/best_model/plot.svg')
+    plt.show()
 
 
 def main():
@@ -10,7 +26,7 @@ def main():
     tuner = mobilenetv2_tuner.tuner
     # tuner.results_summary()  # Descomentar para verificar que sí cargó los resultados obtenidos previamente
 
-    checkpoint_filepath = 'tmp/checkpoint'  # Puede ser una ruta diferente
+    checkpoint_filepath = 'Results/best_model/checkpoint_{epoch:02d}'  # Puede ser una ruta diferente
 
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
     model = tuner.hypermodel.build(best_hps)
@@ -29,12 +45,16 @@ def main():
         save_best_only=True
     )
 
-    model.fit(
+    history = model.fit(
         x=train_generator,
         validation_data=(x_val, y_val),
         epochs=500,
         callbacks=[model_checkpoint_callback]
     )
+
+    plot_history(history)
+    df = pd.DataFrame(history.history)
+    df.to_csv('Results/best_model/metrics.csv')
 
 
 if __name__ == '__main__':
